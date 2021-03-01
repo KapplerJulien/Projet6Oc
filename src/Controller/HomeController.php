76b56240e6811ec6bd054c90ffa30585ac31796c
xibougta <kappler.julien@gmail.com>
2021-03-01 14:24:11 +0100

    Edit some Home files

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,16 +9,12 @@ use Symfony\Component\HttpFoundation\Request;
 use App\Entity\ArticleTriks;
 use App\Entity\ImageTriks;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 
 class HomeController extends AbstractController 
 {
-    private $session;
-
-    public function __construct(SessionInterface $session)
+    public function __construct()
     {
-        $this->session = $session;
     }
 
     /**
@@ -28,29 +24,15 @@ class HomeController extends AbstractController
     {
         // Var
         $paginator_per_page = 5 + $request->query->getInt('paginator_per_page', 0);
-        $imagesArt = array();
         $i = 1;
         $repositoryArticles = $this->getDoctrine()->getRepository(ArticleTriks::class);
-        $repositoryImage = $this->getDoctrine()->getRepository(ImageTriks::class);
 
         // Collect articles from the Database 
         $paginArticles = $repositoryArticles->getPaginArticle($paginator_per_page);
 
-        // Collect image from the Database
-        foreach($paginArticles as $article){
-            // var_dump($article->getId());
-            $imageArt[$i] = $repositoryImage->getImgByArticle($article);
-            // var_dump($imageArt[$i][0]);
-            $i ++;
-        }
-
-        $user = $this->session->get('userId');
-
         return $this->render('home/home.html.twig', [
             "articles" => $paginArticles,
             "next" => min(count($paginArticles), $paginator_per_page),
-            "user" => $user,
-            "imageArt" => $imageArt,
         ]);
     }
 }
